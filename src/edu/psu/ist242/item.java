29fc00f5915a677d92d25e82ad78297bb2cb18b1
ial5049 <36733602+ial5049@users.noreply.github.com>
2018-02-25 18:34:37 -0500

    Create item.java

---
+++ src/edu/psu/ist242/item.java
@@ -0,0 +1,57 @@
+package edu.psu.ist242;
+
+/* 
+  created by irainaL on 2/25/2018
+*/
+
+public class item{
+  
+  public String name;
+  public float price;
+  public String description;
+  public String groups[];
+  
+  public item(String name, float price, String description, String groupd[]){
+    this.name = ""l;
+    this.price = null;
+    this.description = "";
+    this.groups[] = null;
+  }
+    
+  public String getName(){
+    return name;
+  }
+  
+  public void setName(String name){
+    this.name = name;
+  }
+  
+  public float getPrice(){
+    return getPrice;
+  }
+  
+  public void setPrice(float price){
+    this.price = price;
+  }
+  
+  public String getDescription(){
+    return description;
+  }
+  
+  public void setDescription(String description){
+    this.description = description;
+  }
+  
+  public String getGroups[](){
+    return groups[];
+  }
+  
+  public void setGroups(String groups[]){
+    this.groups[] = groups[];
+  }
+  
+  
+  
+}
+  
+  
