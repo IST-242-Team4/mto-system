0ff8d1df608135178c5067a24546bc6d14d2e4a3
cxm1071 <36733706+cxm1071@users.noreply.github.com>
2018-02-27 08:22:30 -0500

    Create Payment.java

---
+++ src/edu/psu/ist242/Payment.java
@@ -0,0 +1,51 @@
+/*
+Project: Team4 Project
+Purpose Details: ordering kiosk
+Course: IST 242
+Author: Charles McKee
+Date Developed: 2-24-2018
+Last Date Changed: 2-24-2018
+ */
+
+package edu.psu.IST242;
+import java.util.*;
+
+public class Payment {
+
+    private int id;
+    private Date paid;
+    private double total;
+    private String details;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getPaid() {
+        return paid;
+    }
+
+    public void setPaid(Date paid) {
+        this.paid = paid;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}
