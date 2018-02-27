2ed8aedc057720a2ac7be629c0e5883b9299f4d6
cxm1071 <36733706+cxm1071@users.noreply.github.com>
2018-02-27 08:25:00 -0500

    Create Login.java

---
+++ src/edu/psu/ist242/Login.java
@@ -0,0 +1,42 @@
+/*
+Project: Team4 Project
+Purpose Details: ordering kiosk
+Course: IST 242
+Author: Charles McKee
+Date Developed: 2-24-2018
+Last Date Changed: 2-24-2018
+ */
+
+
+package edu.psu.IST242;
+
+public class Login {
+
+    private String loginID;
+    private String password;
+    private LoginState state;
+
+    public String getLoginID() {
+        return loginID;
+    }
+
+    public void setLoginID(String loginID) {
+        this.loginID = loginID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LoginState getState() {
+        return state;
+    }
+
+    public void setState(LoginState state) {
+        this.state = state;
+    }
+}
