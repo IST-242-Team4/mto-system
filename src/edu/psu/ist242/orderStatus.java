e1bc60bb10a660e13ae474a55aaf5181e22e4497
Ruijian Li <36863253+Sorrow920@users.noreply.github.com>
2018-02-28 15:21:10 -0500

    Create orderStatus.java

---
+++ src/edu/psu/ist242/orderStatus.java
@@ -0,0 +1,26 @@
+/*
+Project: Team4 Project
+Purpose Details: ordering kiosk
+Course: IST 242
+Author: Ruijian Li
+Date Developed: 2-27-2018
+Last Date Changed: 
+ */
+ 
+ package edu.psu.IST242;
+ 
+ public class orderStatus {
+
+    String [] orderStatus = new String []{"new", "paid", "cancelled", "inProcess", "completed", "shipped"};
+
+
+    public void setStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public int getStatus() {
+        return orderStatus;
+    }
+    
+    
+}//Close
