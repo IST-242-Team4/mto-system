1df9b1330b9a09600df81a57b0cdd17ecdf671f0
Ruijian Li <36863253+Sorrow920@users.noreply.github.com>
2018-02-28 15:57:54 -0500

    Create Search.java

---
+++ src/edu/psu/ist242/Search.java
@@ -0,0 +1,16 @@
+/*
+Project: Team4 Project
+Purpose Details: ordering kiosk
+Course: IST 242
+Author: Ruijian Li
+Date Developed: 2-27-2018
+Last Date Changed: 
+ */
+ 
+ package edu.psu.IST242;
+ 
+ 
+  public class search {
+  
+  }
+  
