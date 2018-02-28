c2d605cfdd54322a92711b264f0a26bf62bf19a7
Ruijian Li <36863253+Sorrow920@users.noreply.github.com>
2018-02-28 15:36:10 -0500

    Create receipt.java

---
+++ src/edu/psu/ist242/receipt.java
@@ -0,0 +1,25 @@
+/*
+Project: Team4 Project
+Purpose Details: ordering kiosk
+Course: IST 242
+Author: Ruijian Li
+Date Developed: 2-27-2018
+Last Date Changed: 
+ */
+ 
+ package edu.psu.IST242;
+ 
+ public class receipt {
+ 
+  public int orderID;
+
+ 
+ public getPrintReceipt(){
+    return orderID;
+  }
+  
+  public void setPrintReceipt(int orderID){
+    this.orderID = orderID;
+  }
+ 
+ }//Close
