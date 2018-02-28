aae2e522c2316fa9713ff9a516161ba62fc5637f
Ruijian Li <36863253+Sorrow920@users.noreply.github.com>
2018-02-28 16:18:20 -0500

    Create worker.java

---
+++ src/edu/psu/ist242/worker.java
@@ -0,0 +1,19 @@
+/*
+Project: Team4 Project
+Purpose Details: ordering kiosk
+Course: IST 242
+Author: Ruijian Li
+Date Developed: 02-27-2018
+Last Date Changed:
+ */
+
+
+package edu.psu.IST242;
+
+public class worker {
+
+    public String fillOrder() {
+       return;
+    }
+
+   }//Close
