10106e5dc035cc3e6b5abf94ce28220d90709ef6
Ruijian Li <36863253+Sorrow920@users.noreply.github.com>
2018-02-28 16:05:43 -0500

    Create Address.java

---
+++ src/edu/psu/ist242/Address.java
@@ -0,0 +1,60 @@
+
+/*
+Project: Team4 Project
+Purpose Details: ordering kiosk
+Course: IST 242
+Author: Ruijian Li
+Date Developed: 2-27-2018
+Last Date Changed: 
+ */
+ 
+ package edu.psu.IST242;
+ import javax.mail.Address;
+ 
+ 
+ public class address {
+ 
+ 
+    private Address street;
+    private String city;
+    private String state;
+    private String zip;
+ 
+ 
+ 
+    public Address getStreet() {
+        return street;
+    }
+
+    public void setStreet(Address street) {
+        this.street = street;
+    }
+    
+    
+        public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+ 
+ 
+      public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+    
+      public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+    
+    
+ }//Close
