dc984c66a115dc1a7a8c27253c5fa7bd44a28577
Ruijian Li <36863253+Sorrow920@users.noreply.github.com>
2018-02-28 16:13:22 -0500

    Create guest.java

---
+++ src/edu/psu/ist242/guest.java
@@ -0,0 +1,17 @@
+/*
+Project: Team4 Project
+Purpose Details: ordering kiosk
+Course: IST 242
+Author: Ruijian Li
+Date Developed: 2-27-2018
+Last Date Changed: 
+ */
+ 
+ package edu.psu.IST242;
+ 
+ public class guest {
+ 
+ 
+ 
+ 
+ }//close
