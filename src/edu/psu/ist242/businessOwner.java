ee843b8c36155053aafad8c759f11265501f3e75
Ruijian Li <36863253+Sorrow920@users.noreply.github.com>
2018-02-28 16:17:16 -0500

    Create businessOwner.java

---
+++ src/edu/psu/ist242/businessOwner.java
@@ -0,0 +1,20 @@
+/*
+Project: Team4 Project
+Purpose Details: ordering kiosk
+Course: IST 242
+Author: Ruijian Li
+Date Developed: 02-27-2018
+Last Date Changed:
+ */
+
+
+package edu.psu.IST242;
+
+
+public class businessOwner {
+
+    public String organizeItem() {
+        
+    }
+
+}
