49e3557e2ade6fbe067de7af0fe89d794d591726
Ruijian Li <36863253+Sorrow920@users.noreply.github.com>
2018-02-28 16:10:47 -0500

    Create loginState.java

---
+++ src/edu/psu/ist242/loginState.java
@@ -0,0 +1,26 @@
+/*
+Project: Team4 Project
+Purpose Details: ordering kiosk
+Course: IST 242
+Author: Ruijian Li
+Date Developed: 2-27-2018
+Last Date Changed: 
+ */
+ 
+ package edu.psu.IST242;
+
+ 
+ public class loginState {
+ 
+ String [] loginState = new String []{"authenticated", "unauthenticated", "disabled"};
+
+
+    public void setLoginState(String loginState) {
+        this.loginState = loginState;
+    }
+
+    public int getLoginState() {
+        return loginState;
+    }
+ 
+ }//close
