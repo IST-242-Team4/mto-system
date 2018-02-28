38c0db64a7ee43f09b1e7eea5124af18055a77b6
Ruijian Li <36863253+Sorrow920@users.noreply.github.com>
2018-02-28 15:44:55 -0500

    Create Page.java

---
+++ src/edu/psu/ist242/Page.java
@@ -0,0 +1,46 @@
+/*
+Project: Team4 Project
+Purpose Details: ordering kiosk
+Course: IST 242
+Author: Ruijian Li
+Date Developed: 2-27-2018
+Last Date Changed: 
+ */
+ 
+ package edu.psu.IST242;
+ 
+ 
+  public class page {
+  
+  public int pageID;
+  public String name;
+  public int permission;
+ 
+  
+  public int getpageID(){
+    return pageID;
+  }
+  
+  public void setPageID(int pageID){
+    this.pageID = pageID;
+  }
+  
+  
+  public String getName(){
+    return name;
+  }
+  
+  public void setName(String name){
+    this.name = name;
+  }
+  
+  
+  public int getPermission(){
+    return permission;
+  }
+  
+  public void setPermission(int permission){
+    this.permission = permission;
+  }
+  
+ }//close
