0de4bbaf35ea2329b5eeb5fa502968cbb8c37d22
cxm1071 <36733706+cxm1071@users.noreply.github.com>
2018-03-01 00:20:46 -0500

    Update Customer.java

--- src/edu/psu/ist242/Customer.java
+++ src/edu/psu/ist242/Customer.java
@@ -10,13 +10,11 @@ Last Date Changed: 2-24-2018
 
 package edu.psu.IST242;
 import java.util.*;
-import java.lang.Object;
-import javax.mail.Address; //MUST HAVE JAVAMAIL API IN JAR FILES
 
 public class Customer {
 
     private int id;
-    private Address shipAddress; //IF THIS ISNT WORKING YOU DIDNT PUT JAVAMAIL API IN JAR FILES
+    private String shipAddress; //IF THIS ISNT WORKING YOU DIDNT PUT JAVAMAIL API IN JAR FILES
     private String phone;
     private String email;
     private int permission;
@@ -29,11 +27,11 @@ public class Customer {
         return id;
     }
 
-    public Address getShipAddress() {
+    public String getShipAddress() {
         return shipAddress;
     }
 
-    public void setShipAddress(Address shipAddress) {
+    public void setShipAddress(String shipAddress) {
         this.shipAddress = shipAddress;
     }
 
