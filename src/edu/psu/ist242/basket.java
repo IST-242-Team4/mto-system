92e9a99751cd2af4255a41a14cf9dcd575478279
ial5049 <36733602+ial5049@users.noreply.github.com>
2018-02-28 20:08:14 -0500

    Update basket.java

--- src/edu/psu/ist242/basket.java
+++ src/edu/psu/ist242/basket.java
@@ -4,30 +4,30 @@ package edu.psu.ist242;
   created by irainaL on 2/25/2018
 */
 
-public class basket{
-  
-  public int basketID;
-  public date created;
-  
-  public basket(int basketID, date created){
-    this.basketID = null;
+public class Basket{
+
+  private int basketID;
+  private String created;
+
+  public Basket(int basketID, String created){
+    this.basketID = 0;
     this.created = null;
   }
-    
+
   public int getBasketID(){
     return basketID;
   }
-  
+
   public void setBasketID(int basketID){
     this.basketID = basketID;
   }
-  
-  public date getCreated(){
+
+  public String getCreated(){
     return created;
   }
-  
-  public void setCreated(date created){
+
+  public void setCreated(String created){
     this.created = created;
   }
-  
+
 }
