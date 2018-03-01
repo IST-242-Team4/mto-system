127689b7b573ca6413f6220ff38d09db985ef4c6
ial5049 <36733602+ial5049@users.noreply.github.com>
2018-02-28 20:12:48 -0500

    Update lineItem.java

--- src/edu/psu/ist242/lineItem.java
+++ src/edu/psu/ist242/lineItem.java
@@ -4,41 +4,42 @@ package edu.psu.ist242;
   created by irainaL on 2/25/2018
 */
 
-public class lineItem{
-  
+public class LineItem{
+
   public int id;
   public String item;
   public int qty;
-  
-  public lineItem(int id, String item, int qty){
-    this.id = null;
+
+  public LineItem(int id, String item, int qty){
+    this.id = 0;
     this.item = "";
-    this.qty = null;
+    this.qty = 0;
   }
-    
+
   public int getId(){
     return id;
   }
-  
+
   public void setId(int id){
     this.id = id;
   }
-  
-  public int getItem(){
-    return getItem;
+
+  public String getItem(){
+    return item;
   }
-  
+
   public void setItem(String item){
     this.item = item;
   }
-  
-  public int getQty(){
+
+   public int getQty(){
     return qty;
   }
-  
+
   public void setQty(int qty){
     this.qty = qty;
   }
+
   
 }
   
