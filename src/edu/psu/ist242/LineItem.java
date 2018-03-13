db1b6e3bc17de92b85f1176a2c668320813aae96
ial5049 <36733602+ial5049@users.noreply.github.com>
2018-03-13 08:05:03 -0400

    Update LineItem.java

--- src/edu/psu/ist242/LineItem.java
+++ src/edu/psu/ist242/LineItem.java
@@ -1,8 +1,14 @@
-package edu.psu.ist242;
-
-/* 
-  created by irainaL on 2/25/2018
+/*
+Project: MTO System
+Purpose Details: Creating a LineItem class
+Course: IST 242
+Author: Iraina Lambe
+Date Developed: 2/25/2018
+Last Date Changed:3/13/2018
+Rev:
 */
+
+package edu.psu.ist242;
 
 public class LineItem{
 
