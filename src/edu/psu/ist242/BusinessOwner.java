441a31aea3702c4f2e0d58651ed19e8d73940417
Ruijian Li <36863253+Sorrow920@users.noreply.github.com>
2018-03-13 08:03:29 -0400

    Update BusinessOwner.java

--- src/edu/psu/ist242/BusinessOwner.java
+++ src/edu/psu/ist242/BusinessOwner.java
@@ -13,8 +13,15 @@ package edu.psu.ist242;
 
 public class BusinessOwner {
 
-    public String organizeItem() {
-        //NEED WORK HERE
+    public String organizeOrder;
+    public String organizePeople;
+  
+    public String toOrganizeOrder(String organizeOrder) {
+        return organizeOrder; //NEED WORK HERE
+    }
+
+    public String toOrganizePeople(String organizePeople) {
+      return organizePeople; //NEED WORK HERE
     }
 
 }
