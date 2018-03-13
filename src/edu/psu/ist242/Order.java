b4c870843c023771e7299b00be129442664e2c05
ial5049 <36733602+ial5049@users.noreply.github.com>
2018-03-13 08:06:01 -0400

    Update Order.java

--- src/edu/psu/ist242/Order.java
+++ src/edu/psu/ist242/Order.java
@@ -1,6 +1,15 @@
+/*
+Project: MTO System
+Purpose Details: Creating an order class
+Course: IST 242
+Author: Iraina Lambe
+Date Developed: 2/25/2018
+Last Date Changed:3/13/2018
+Rev:
+*/
+
 package edu.psu.ist242;
 
-//not completed
 public class Order{
 
   public int id;
