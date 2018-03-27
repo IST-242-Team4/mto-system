ff0ebe9c63274a3de2231603386a4514b9f92bb7
cxm1071 <36733706+cxm1071@users.noreply.github.com>
2018-03-27 13:01:04 -0400

    Update Account.java

--- src/edu/psu/ist242/Account.java
+++ src/edu/psu/ist242/Account.java
@@ -6,6 +6,12 @@ Author: Charles McKee
 Date Developed: 2-24-2018
 Last Date Changed: 2-24-2018
  */
+/**
+*<h1>Account.java</h1>
+*This Class sets up the Account information
+*<p>
+*@author Charles McKee
+*/
 
 
 package edu.psu.ist242;
@@ -13,49 +19,42 @@ import java.util.*;
 
 
 public class Account {
-
+    /** Declared Variables */
     private int id;
-    private String billingAddress; //IF THIS ISNT WORKING YOU DIDNT PUT JAVAMAIL API IN JAR FILES
+    private String billingAddress;
     private Boolean isClosed;
     private Date opened;
     private Date closed;
-
-
+    
+    /**Accessor Methods */
     public int getId() {
         return id;
     }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getBillingAddress() {
         return billingAddress;
     }
-
-    public void setBillingAddress(String billingAddress) {
-        this.billingAddress = billingAddress;
-    }
-
     public Boolean getisClosed() {
         return isClosed;
+    }
+    public Date getClosed() {
+        return closed;
+    }
+    public Date getOpened() {
+        return opened;
+    }
+    /**mutator Methods*/
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setBillingAddress(String billingAddress) {
+        this.billingAddress = billingAddress;
     }
     public void setisClosed(Boolean closed) {
         isClosed = closed;
     }
-
-    public Date getClosed() {
-        return closed;
-    }
-
     public void setClosed(Date closed) {
         this.closed = closed;
     }
-
-    public Date getOpened() {
-        return opened;
-    }
-
     public void setOpened(Date opened) {
         this.opened = opened;
     }
