08c9287ad764ff0c1db3c1c8c043e326a3db3d91
cxm1071 <36733706+cxm1071@users.noreply.github.com>
2018-03-27 13:10:00 -0400

    Update Login.java

--- src/edu/psu/ist242/Login.java
+++ src/edu/psu/ist242/Login.java
@@ -6,36 +6,39 @@ Author: Charles McKee
 Date Developed: 2-24-2018
 Last Date Changed: 2-24-2018
  */
+/**
+*<h1> Login Class</h1>
+*This Class sets Login information
+*<p>
+*@author Charles McKee
+*/
 
 
 package edu.psu.ist242;
 
 public class Login {
-
+    /**Declared variables*/
     private String loginID;
     private String password;
     private String state;
 
+    /**Accessor methods*/
     public String getLoginID() {
         return loginID;
     }
-
-    public void setLoginID(String loginID) {
-        this.loginID = loginID;
-    }
-
     public String getPassword() {
         return password;
     }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getState() {
         return state;
     }
-
+    /**mutator methods*/
+    public void setLoginID(String loginID) {
+        this.loginID = loginID;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
     public void setState(String state) {
         this.state = state;
     }
