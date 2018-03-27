fc4d277a27a57c5b5cf2a18a2009b6d4bc5a88d2
cxm1071 <36733706+cxm1071@users.noreply.github.com>
2018-03-27 13:11:57 -0400

    Update Payment.java

--- src/edu/psu/ist242/Payment.java
+++ src/edu/psu/ist242/Payment.java
@@ -6,45 +6,46 @@ Author: Charles McKee
 Date Developed: 2-24-2018
 Last Date Changed: 2-24-2018
  */
+/**
+*<h1> Payment Class</h1>
+*This Class sets Payment information
+*<p>
+*@author Charles McKee
+*/
 
 package edu.psu.ist242;
 import java.util.*;
 
 public class Payment {
-
+    /**Declared Variables*/
     private int id;
     private Date paid;
     private double total;
     private String details;
 
+    /**Accessor Methods*/
     public int getId() {
         return id;
     }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public Date getPaid() {
         return paid;
     }
-
-    public void setPaid(Date paid) {
-        this.paid = paid;
-    }
-
     public double getTotal() {
         return total;
     }
-
-    public void setTotal(double total) {
-        this.total = total;
-    }
-
     public String getDetails() {
         return details;
     }
-
+    /**Mutator Methods*/
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setPaid(Date paid) {
+        this.paid = paid;
+    }
+    public void setTotal(double total) {
+        this.total = total;
+    }
     public void setDetails(String details) {
         this.details = details;
     }
