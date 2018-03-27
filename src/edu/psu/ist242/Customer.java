a2f15bcfcf4fad170a0a839b86cf82f27c4a90b5
cxm1071 <36733706+cxm1071@users.noreply.github.com>
2018-03-27 13:05:43 -0400

    Update Customer.java

--- src/edu/psu/ist242/Customer.java
+++ src/edu/psu/ist242/Customer.java
@@ -6,6 +6,12 @@ Author: Charles McKee
 Date Developed: 2-24-2018
 Last Date Changed: 2-24-2018
  */
+/**
+*<h1> Customer Class</h1>
+*This Class sets customer information
+*<p>
+*@author Charles McKee
+*/
 
 
 package edu.psu.ist242;
@@ -13,48 +19,43 @@ import java.util.*;
 
 public class Customer {
 
+    /**Declared Variables*/
     private int id;
-    private String shipAddress; //IF THIS ISNT WORKING YOU DIDNT PUT JAVAMAIL API IN JAR FILES
+    private String shipAddress; 
     private String phone;
     private String email;
     private int permission;
 
-    public void setId(int id) {
-        this.id = id;
-    }
 
+    /**Accessor methods*/
     public int getId() {
         return id;
     }
-
     public String getShipAddress() {
         return shipAddress;
     }
-
-    public void setShipAddress(String shipAddress) {
-        this.shipAddress = shipAddress;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
     public String getEmail() {
         return email;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public String getPhone() {
+        return phone;
     }
-
     public int getPermission() {
         return permission;
     }
-
+    /**Mutator Methods*/
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public void setShipAddress(String shipAddress) {
+        this.shipAddress = shipAddress;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
     public void setPermission(int permission) {
         this.permission = permission;
     }
