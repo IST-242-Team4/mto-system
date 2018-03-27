0c42050a5a1e3335eeb62027fb9dd9e783a892aa
cxm1071 <36733706+cxm1071@users.noreply.github.com>
2018-03-27 13:08:15 -0400

    Update Employee.java

--- src/edu/psu/ist242/Employee.java
+++ src/edu/psu/ist242/Employee.java
@@ -6,64 +6,61 @@ Author: Charles McKee
 Date Developed: 2-24-2018
 Last Date Changed: 2-24-2018
  */
+/**
+*<h1> Employee Class</h1>
+*This Class sets Employee information
+*<p>
+*@author Charles McKee
+*/
 
 
 package edu.psu.ist242;
 import java.util.*;
 
 public class Employee {
-
+    /**Declared Variables*/
     private int id;
-    private String shipAddress; //IF THIS ISNT WORKING YOU DIDNT PUT JAVAMAIL API IN JAR FILES
+    private String shipAddress;
     private String phone;
     private String email;
     private String title;
     private int permission;
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
+    /**Accessor Methods*/
     public int getId() {
         return id;
     }
-
-    public String getShipAddress() {
-        return shipAddress;
-    }
-
-    public void setShipAddress(String shipAddress) {
-        this.shipAddress = shipAddress;
-    }
-
     public String getPhone() {
         return phone;
     }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
+    public String getShipAddress() {
+        return shipAddress;
     }
-
     public String getEmail() {
         return email;
     }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getTitle() {
         return title;
     }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public int getPermission() {
         return permission;
     }
-
+    /**Mutator Methods*/
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setShipAddress(String shipAddress) {
+        this.shipAddress = shipAddress;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
     public void setPermission(int permission) {
         this.permission = permission;
     }
