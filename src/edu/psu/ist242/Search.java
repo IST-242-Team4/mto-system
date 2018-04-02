92eb6797d7e7ef8715d13b537f60bd0eb74294e0
Ruijian Li <36863253+Sorrow920@users.noreply.github.com>
2018-04-02 17:55:21 -0400

    Update Search.java

--- src/edu/psu/ist242/Search.java
+++ src/edu/psu/ist242/Search.java
@@ -11,6 +11,34 @@ Last Date Changed:
  
  
   public class Search {
-  
+ 
+  /* Declared Variables */
+  public String customerInput;
+  public String searching;
+  public int permission;
+ 
+  /*Accessor methods*/
+  public String getCustomerInput(){
+    return customerInput;
   }
+  public String getSearching(){
+    return searching;
+  }
+  public int getPermission(){
+    return permission;
+  }
+  
+  /*Mutator Methods*/  
+  public void setCustomerInput(int customerInput){
+    this.customerInput = customerInput;
+  }
+  public void setSearching(String searching){
+    this.searching = searching;
+  }
+  public void setPermission(int permission){
+    this.permission = permission;
+  }
+  
+ }//close
+
   
