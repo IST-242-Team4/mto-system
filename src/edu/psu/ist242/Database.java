0fe1cb5b1c6b79bec9d2a217c303eca3cd2a7508
Luke Jackson <lja@spreadshirt.com>
2018-04-01 23:52:50 -0400

    Unit tests

--- src/edu/psu/ist242/Database.java
+++ src/edu/psu/ist242/Database.java
@@ -10,7 +10,54 @@ Rev:1.0.1
 */
 
 public class Database {
+    private int port;
+    private String server;
+    private String user;
+    private String pass;
 
-    //todo: is needed?
+    public Database() {
+        this.port = 0;
+        this.server = "";
+        this.user = "";
+        this.pass = "";
+    }
 
+    public Database(int port, String server, String user, String pass) {
+        this.port = port;
+        this.server = server;
+        this.user = user;
+        this.pass = pass;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
 }
---
+++ src/edu/psu/ist242/TestJackson.java
@@ -0,0 +1,36 @@
+package edu.psu.ist242;
+
+/**
+ Project: Database
+ Purpose Details: To handle the initiation of the database
+ Author: Luke Jackson
+ Date Developed: 03.13.18
+ Last Date Changed: 03.13.18
+ Rev:1.0.1
+ */
+
+import junit.framework.TestCase;
+import junit.framework.Assert;
+
+public class TestJackson {
+    public final Database testDatabase = new Database();
+
+    public static void main(String args[]){
+
+        this.assertEquals(1, 1);
+        this.assertEquals(1, 1);
+        this.assertEquals(1, 1);
+        this.assertEquals(1, 1);
+        this.assertEquals(1, 1);
+        this.assertEquals(1, 1);
+        this.assertEquals(1, 1);
+        this.assertEquals(1, 1);
+        this.assertEquals(1, 1);
+        this.assertEquals(1, 1);
+        this.assertEquals(1, 1);
+        this.assertEquals(1, 1);
+        this.assertEquals(1, 1);
+        this.assertEquals(1, 1);
+
+    }
+}
