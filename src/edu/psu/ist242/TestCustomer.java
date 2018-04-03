37a9dd347a842d740c175bf79fd38a413916715f
cxm1071 <36733706+cxm1071@users.noreply.github.com>
2018-04-03 09:07:42 -0400

    Create TestCustomer.java

---
+++ src/edu/psu/ist242/TestCustomer.java
@@ -0,0 +1,49 @@
+/*
+Project: Team4 Project
+Purpose Details: ordering kiosk
+Course: IST 242
+Author: Charles McKee
+Date Developed: 3-27-2018
+Last Date Changed: 3-27-2018
+ */
+/**
+ *<h1> TestMcKee Class</h1>
+ *This Class tests Account, Employee, Customer, Login, and Payment classes
+ *<p>
+ *@author Charles McKee
+ */
+
+package edu.psu.ist242;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class TestMcKee {
+    public static void main(String[] args) throws Exception{
+        testCustomer();
+        }
+            public static void testCustomer() {
+        Customer cust = new Customer();
+        cust.setId(1);
+        cust.setShipAddress("123 A St");
+        cust.setPhone("111-111-1111");
+        cust.setEmail("abc@gmail.com");
+        cust.setPermission(1);
+        if(cust.getId()==1) {
+            if(cust.getShipAddress().equals("123 A St")) {
+                if(cust.getPhone().equals("111-111-1111")) {
+                    if(cust.getEmail().equals("abc@gmail.com")) {
+                        if(cust.getPermission()==1){
+                            System.out.println("Test on Customer: Passed");
+                        }
+                        else { System.out.println("Test on get/set Permission: Failed"); }
+                    }
+                    else { System.out.println("Test on get/set Email: Failed"); }
+                }
+                else { System.out.println("Test on get/set Phone: Failed"); }
+            }
+            else { System.out.println("Test on get/set ShipAddress: Failed"); }
+        }
+        else { System.out.println("Test on get/set ID: Failed"); }
+    }
+    }
